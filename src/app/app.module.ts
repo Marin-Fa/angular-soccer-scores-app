c40c7415a1d021330b6cea14f86212b19dac75d3
Marin-Fa <marin.fayolle@gmail.com>
2020-12-16 12:12:37 +0100

    Creating Components and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MatchListComponent } from './ressources/matches/match-list/match-list.component';
+import { MatchDetailComponent } from './ressources/matches/match-detail/match-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MatchListComponent,
+    MatchDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
